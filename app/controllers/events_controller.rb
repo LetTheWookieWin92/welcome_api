75fe26c4d902090ccf8f94cd212fb26bf7fe308d
Geoff Reid <geoff_reid@outlook.com>
2022-07-28 18:21:00 +1000

    Added show, update and destroy event methods with rescue steps and messages

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,7 @@
 class EventsController < ApplicationController
+    
+    # Runs the set event before running show, update or delete (see method comment below)
+    before_action :set_event, only: [:show, :update, :destroy]
 
     # Get all events
     def index
@@ -13,11 +16,50 @@ class EventsController < ApplicationController
         begin
             @event = Event.create(event_params)
         rescue
-            render status: :unprocessable_entity
+            # Pass back unsuccessful event create status
+            render json: {error: "Event not created; missing required parameters"}, status: :unprocessable_entity
         else
-            render json: @event, status: 201 # Pass back successful event creation status
+            # Pass back successful event create status
+            render json: @event, status: 201 
         end      
 
+    end
+
+    # Returns a specific event
+    def show
+        render json: @event
+    end
+
+    # Updates a specific event
+    def update
+        
+        begin
+            @event.update(event_params)
+        rescue
+            # Pass back unsuccessful event update status
+            render json: {error: "Event could not be updated; missing required parameters"}, status: :unprocessable_entity
+        else
+            # Pass back successful event update status
+            render json: @event, status: 201 
+        end  
+    end
+
+    # Deletes a specific event
+    def destroy
+        
+        @event.delete
+        render status: 204
+        
+    end
+
+    # Finds the desired event before running the show or update method
+    def set_event
+        begin
+            @event = Event.find(params[:id])
+        rescue
+            render json: {error: "Event not found"}, status: 404
+        end
+        
     end
 
     # Defines the parameters that are permitted to be sent when creating an event
