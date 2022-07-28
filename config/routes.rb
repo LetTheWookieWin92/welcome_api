5cfc74bd1146f42547850cac0a04458b7c00be0e
Geoff Reid <geoff_reid@outlook.com>
2022-07-28 14:15:46 +1000

    Created simple routes for events API, including read, create, show, update and destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+    scope '/api' do
+        get '/events', to: 'events#index'           # Reads all events
+        post '/events', to: 'events#create'         # Creates events
+        get '/events/:id', to: 'events#show'        # Read specific event with id
+        put '/events/:id', to: 'events#update'       # Create or update an event with id
+        delete '/events/:id', to: 'events#destroy'  # Destroy an event with id
+    end
+
 end
