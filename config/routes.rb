680aea42c991aee51e20b8d99f36b37ffb40cf0d
Geoff Reid <geoff_reid@outlook.com>
2022-07-29 17:05:29 +1000

    Installed cors-rack gem to allow client and api integration. Added extra test http requests to client.http. Added root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
-    scope '/api' do
-        get '/events', to: 'events#index'           # Reads all events
-        post '/events', to: 'events#create'         # Creates events
-        get '/events/:id', to: 'events#show'        # Read specific event with id
-        put '/events/:id', to: 'events#update'      # Create or update an event with id
-        delete '/events/:id', to: 'events#destroy'  # Destroy an event with id
-    end
+    root to: 'events#index'
+    get '/events', to: 'events#index'           # Reads all events
+    post '/events', to: 'events#create'         # Creates events
+    get '/events/:id', to: 'events#show'        # Read specific event with id
+    put '/events/:id', to: 'events#update'      # Create or update an event with id
+    delete '/events/:id', to: 'events#destroy'  # Destroy an event with id
+
 
 end
